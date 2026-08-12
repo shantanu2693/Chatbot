7628223f508be916a65bd220c1b3850fe1c19582
Shantanu Sharma <shantanu24sharma@gmail.com>
2026-08-11 20:46:28 -0400

    Resolved message layout issue between Langchain and Hugging Face

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from tokenize import Comment
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 from huggingface_hub import InferenceClient
+import streamlit as st
 from dotenv import load_dotenv
 import os
 
@@ -8,6 +10,7 @@ load_dotenv()
 
 client = InferenceClient(
     api_key=os.environ["HF_TOKEN"],
+    provider="novita"
 )
 
 chatprompttemplate = ChatPromptTemplate.from_messages([
@@ -23,9 +26,28 @@ while True:
     if user_input.lower() == "exit":
         break;
     prompt = chatprompttemplate.invoke({"input": user_input, "chat_history": chat_history_content})
+
+    listOfMessages = []
+    """"
+    for message in prompt.to_messages():
+        listOfMessages.append({"role": message.type, "content": message.content})
+    """
+    
+    for message in prompt.to_messages(): 
+        if message.type == "system": 
+            role = "system" 
+        elif message.type == "human": 
+            role = "user"
+        elif message.type == "ai": 
+            role = "assistant" 
+        else: 
+            continue 
+        
+        listOfMessages.append({ "role": role, "content": message.content })
+   
     completion = client.chat.completions.create(
-        model="deepseek-ai/DeepSeek-V4-Flash-0731:novita",
-        messages=prompt
+        model="deepseek-ai/DeepSeek-V4-Flash-0731",
+        messages=listOfMessages
     )
 
     result = completion.choices[0].message.content
@@ -33,4 +55,3 @@ while True:
 
     chat_history_content.append(HumanMessage(content=user_input))
     chat_history_content.append(AIMessage(content=result))
-
